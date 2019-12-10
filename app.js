9ae9984630cfc69a70b224ed3c2046d15a359e13
Spronjee <i.chukhlebov6530@gmail.com>
2019-12-10 20:15:44 +0200

    first comit about testing on JS

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+let Bodya = {
+    color: 'NotWhite',
+    cover: 'skin',
+    facultet: '123'
+}
+
+
+
+//let answer = prompt("Are you 18?","");
+
+let incr = 10,
+    decr = 10; 
+
+    incr++;
+    decr--;
+console.log(5%2);
+console.log(decr); 
+
+let isChecked = true,
+    isClosed = true;
+    console.log(isChecked || isClosed)
+
