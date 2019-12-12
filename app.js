23960688bcb77270cb9517cc477fe65678eba139
Spronjee <i.chukhlebov6530@gmail.com>
2019-12-13 00:26:20 +0200

    add another code about function

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 'use strict'
-let Bodya = {
-    color: 'NotWhite',
-    cover: 'skin',
-    facultet: '123'
+let money = prompt('Ваш бюджет на месяц?', '');
+console.log(money);
+let time = prompt('Введите дату в формате YYY-MM-DD','');
+console.log(time);
+let AppObject = {
+    money,
+    time,
+    expenses: {
+        1 :prompt('Введите обязательную статью расходов в этом месяце', ''),
+        2 :prompt('Во сколько обойдется?','')
+    },
+    optionalExpenses: {},
+    income: [],
+    savings : true
 }
 
-
-
-//let answer = prompt("Are you 18?","");
-
-let incr = 10,
-    decr = 10; 
-
-    incr++;
-    decr--;
-console.log(5%2);
-console.log(decr); 
-
-let isChecked = true,
-    isClosed = true;
-    console.log(isChecked || isClosed)
-console.log('wiw');
+let answer = prompt('Введите обязательную статью расходов в этом месяце', '');
+let answer2 = prompt('Во сколько обойдется?','');
+//AppObject.expenses = answer,answer2;
+//AppObject.expenses = prompt('Во сколько обойдется?','');
+//AppObject.expenses = prompt('Введите обязательную статью расходов в этом месяце','');
+console.log(AppObject.expenses['1'],['2']);
---
+++ conditionals.js
@@ -0,0 +1,31 @@
+/*let calc = function calc(a,b)
+{
+    return (a+b);
+}
+*/
+
+let calc =(a,b) => {a+b}
+
+console.log(calc(3,4));
+
+console.log(calc(8,4));
+
+function retVar()
+{
+    let num = 50;
+    return num;
+}
+let anotherNum = retVar();
+
+console.log(anotherNum);
+
+let str = 'text';
+console.log(str.length);
+
+console.log(str.toUpperCase()) ;
+console.log(str.toLowerCase());
+
+let twelwe = '12.2';
+console.log(Math.round(twelwe));
+
+
