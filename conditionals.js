dc50a134e0039439e2d2dbdb195e1540e8bc3fb8
Spronjee <i.chukhlebov6530@gmail.com>
2020-01-06 22:37:30 +0200

    showing how to work OOP

--- conditionals.js
+++ conditionals.js
@@ -7,10 +7,22 @@
     arr = ans.split(',');
     console.log(arr);*/
 
-    let arr = [1,2,31,3],
+    /*let arr = [1,2,31,3],
         i = arr.sort(compareNum);
 
         function compareNum(a,b){
             return a - b ;
         }
-        console.log(arr);
+        console.log(arr);*/
+    
+        let soldier = {
+            healh: 400,
+            armor: 100
+        }
+        let john = {
+            healh:100
+        }
+        john.__proto__ = soldier;
+
+        console.log(john);
+        console.log(jonh.armor);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,13 @@
-let arr = ['first',2,3,'four'];
-console.log(arr.length);
-
-
-//for(let i =0; i < arr.length; i++){
-//    console.log(arr[i]);
-//}
-
-arr.forEach(function(item, i,arr, mass){
-    console.log(i + ': ' + item + '(массив: ' + mass + ')')
-});
-
 console.log(arr);
+    
+        let soldier = {
+            healh: 400,
+            armor: 100
+        }
+        let john = {
+            healh:100
+        }
+        john.__proto__ = soldier;
+
+        console.log(john);
+        console.log(jonh.armor);
